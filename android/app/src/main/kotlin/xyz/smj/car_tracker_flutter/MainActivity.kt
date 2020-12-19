b2503c28591d7c6d118a9ea1695592d4a63d856f
S. M. JAHANGIR <smjxpro@gmail.com>
2020-12-19 08:29:52 +0600

    initial commit

---
+++ android/app/src/main/kotlin/xyz/smj/car_tracker_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package xyz.smj.car_tracker_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:car_tracker_flutter/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Car',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
