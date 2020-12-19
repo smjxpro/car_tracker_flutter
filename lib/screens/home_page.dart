8c68d692614fa5ab0405579f4723d6d7abddbd6d
S. M. JAHANGIR <smjxpro@gmail.com>
2020-12-19 12:09:53 +0600

    added google maps with marker and direction

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +9,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Location location = Location();
+
+  static LatLng _initialPosition = LatLng(23.8103, 90.4125);
+
+  static LatLng _currentPosition = _initialPosition;
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  double _direction;
+  BitmapDescriptor carIcon;
+  Set<Marker> markers = Set();
+
+  Future<void> _goToCurrentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: _currentPosition,
+          zoom: 14.4746,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(
+                platform: TargetPlatform.android, size: Size(16, 16)),
+            "assets/images/car_icon.png")
+        .then((value) => carIcon = value);
+
+    location.onLocationChanged.listen((event) {
+      _goToCurrentLocation();
+      setState(() {
+        _currentPosition = LatLng(event.latitude, event.longitude);
+        _direction = event.heading;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    markers.addAll([
+      Marker(
+        markerId: MarkerId('value'),
+        position: _currentPosition,
+        icon: carIcon,
+        flat: true,
+        rotation: _direction,
+        infoWindow: InfoWindow(
+          title: 'Car',
+          snippet: 'Lorem ipsum',
+        ),
+      )
+    ]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Homepage'),
       ),
-      body: Center(),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        zoomControlsEnabled: false,
+        initialCameraPosition: CameraPosition(
+          target: _currentPosition,
+          zoom: 14.4746,
+        ),
+        markers: markers,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        compassEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToCurrentLocation,
+        child: Icon(Icons.location_on),
+      ),
     );
   }
 }
